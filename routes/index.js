473e2e08ae1b41f19c60d264a3fccfd68dbf822d
Projudah <projudah@gmail.com>
2019-01-16 15:36:54 -0500

    backend work in progress

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,8 +8,31 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-	console.log("CHOICKEENEENENENEEN")
-	res.render('index', { title: 'Toronto Waste Lookup' });
+	request.get("https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000", function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var data = JSON.parse(body);
+			search(req.body.search, data, function(hits){
+				console.log(hits);
+				res.render('index', { title: 'Toronto Waste Lookup' });
+			});
+		}
+	});
 });
+
+function search(searchterm, data,callback){
+	var terms = searchterm.split(" ");
+	var hits = [];
+
+	for(var elem=0; elem < data.length; elem++){
+		keyword = data[elem].keywords.split(" ");
+		for(var term=0; term< terms.length; term++){
+			if(keyword.includes(terms[term])){
+				hits.push(data[elem])
+			}
+		}
+	}
+
+	callback(hits)
+}
 
 module.exports = router;
