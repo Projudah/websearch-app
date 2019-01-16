0c1eb68b34cc899fbdda135a8e212d8939c6bb72
Projudah <projudah@gmail.com>
2019-01-16 00:41:50 -0500

    search bar complete finally

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Toronto Waste Lookup' });
 });
 
+router.post('/', function(req, res, next) {
+  res.render('index', { title: 'Toronto Waste Lookup' });
+});
+
 module.exports = router;
